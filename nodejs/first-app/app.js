53e5cae74c28478bd3d099315410ef55c3769cf8
wojciechkubiak <wgkubiak@gmail.com>
2020-11-11 21:44:38 +0100

    NodeJS POST writeFile handler

--- nodejs/first-app/app.js
+++ nodejs/first-app/app.js
@@ -1,7 +1,10 @@
 const http = require('http');
+const fs = require('fs');
+const { Buffer } = require('buffer');
 
 const server = http.createServer((req, res) => {
     const url = req.url;
+    const method = req.method;
 
     res.setHeader('Content-Type', 'text/html');
 
@@ -11,6 +14,23 @@ const server = http.createServer((req, res) => {
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
         res.write('</html>')
         return res.end();
+    }
+
+    if(url === "/message" && method === "POST") {
+        const body = [];
+
+        req.on('data', chunk => {
+            body.push(chunk);
+        });
+        return req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFile('message.txt', message, error => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+        })
     }
 
     res.write('<html>')
